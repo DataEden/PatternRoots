d376ca5d1f0593892526abe1cfee0e13aa53e0e9
DataEden <160447213+DataEden@users.noreply.github.com>
2025-04-19 09:26:55 -0400

    Added code to handle file/io arguements.

--- Regular-Expsression-Projects/coursera-re-project/Files/coursera/RegularExpressions/app.py
+++ Regular-Expsression-Projects/coursera-re-project/Files/coursera/RegularExpressions/app.py
@@ -2,6 +2,19 @@ import sys
 import os
 import regex
 
+if len(sys.argv) != 3:
+    raise ValueError("Please enter a valid input and output file name.")
+
+fname = sys.argv[1]
+
+if not os.path.isfile(fname):
+    raise ValueError("File does not exist.")
+
+with open(fname, mode='r', encoding='utf8') as f:
+    contacts = f.readlines()
+
+for str in contacts:
+    cstr = regex.regex_email(str)
 
 
 
