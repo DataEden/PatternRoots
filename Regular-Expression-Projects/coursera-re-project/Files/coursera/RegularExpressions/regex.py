aaa2e3e1d88e178204c65cc46c79c9eb743a9a62
DataEden <160447213+DataEden@users.noreply.github.com>
2025-04-27 09:55:02 -0400

    Removed docstring draft on lne 39.

--- Regular-Expression-Projects/coursera-re-project/Files/coursera/RegularExpressions/regex.py
+++ Regular-Expression-Projects/coursera-re-project/Files/coursera/RegularExpressions/regex.py
@@ -36,14 +36,7 @@ def regex_email(str)-> str:
             print('invalid email')
               
     return 'invalid email' 
-    """
-    This function takes a string and returns True if it is a valid email address, otherwise False.
-    A valid email address is defined as:
-    - It starts with a letter (a-z, A-Z)
-    - It contains an '@' symbol followed by a domain name (letters, numbers, hyphens, and dots are allowed)
-    - The domain name must end with a top-level domain (TLD) of at least 2 characters (e.g., .com, .org, .net)
-    """
-   
+    
        
 
 
