b78fad6315b4bfd24817837f97a7a7888bc15540
DataEden <160447213+DataEden@users.noreply.github.com>
2025-04-27 10:19:26 -0400

    Added comments

--- Regular-Expression-Projects/coursera-re-project/Files/coursera/RegularExpressions/tests/project_test.py
+++ Regular-Expression-Projects/coursera-re-project/Files/coursera/RegularExpressions/tests/project_test.py
@@ -1,23 +1,21 @@
 import unittest   # The test framework
-import regex as rx
+import regex as rx # Import the regex module to use the regex_email function
 
+
+# Class contains test cases for the regex_email function
 class Project_Test(unittest.TestCase):
     def test_regx(self):
         str = 'email:d_funk@example.com'
         expected = 'email:d_funk@example.com'
         actual = rx.regex_email(str)
         self.assertEqual(expected, actual) # Test with a valid email address
-        # Test with an invalid email address
-        #str = "email:flindo@.com"
-
-        # Test with a string that does not contain an email address
-
-        #
+         
+        # function to test the regex_email function
     def test_nopfx(self):
             str = 'd_funk@example.com'
             expected = 'email:d_funk@example.com'
             actual = rx.regex_email(str)
-            self.assertEqual(expected, actual)
+            self.assertEqual(expected, actual) # Test with a valid email address without prefix
 
 
 
